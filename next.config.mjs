29948e921f5862ece1fe425df2c4bb72e4cd60a2
2023emi <you@example.com>
2024-05-21 07:57:36 +0530

    01

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// const nextConfig = {
+//   output: 'export',
+// }
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
